9d452e31de9e86804223da72f5ed93abb993830e
H0-0H <1749883921@qq.com>
2018-06-05 21:23:05 +0800

    与chm合并后文件，冲突已经解决，完美版，有hewm上传

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from app01.views import course,article,account
+
+urlpatterns = [
+    url(r'^course/$', course.CourseView.as_view({'get':'list'})),
+    url(r'^article/$', article.ArticleView.as_view({'get':'list'})),
+    url(r'^course/(?P<pk>\d+)/$', course.CourseView.as_view({'get':'retrieve'})),
+    url(r'^login/$',account.AuthView.as_view()),
+	1
+	341234234123412341241234
+]
